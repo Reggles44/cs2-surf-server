47c838b11e52b92b715c0e18c563422988e4e066
Reggles <reginaldbeakes@gmail.com>
2026-04-27 17:38:57 -0400

    Moving to config in git

--- create_tgz.sh
+++ create_tgz.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-# function setup::cssharp {
-#   curl https://github.com/roflmuffin/CounterStrikeSharp/releases/latest/download/counterstrikesharp-linux-1.0.367.zip -o /tmp/cssharp.zip
-#   unzip /tmp/cssharp.zip
-#   cp /tmp/cssharp/
-#   
-# }
-
-
-
-# Steps
-# 1. Install CounterStrikeSharp
-# 2. 
-tar -czvf cs2.cfg.tgz --exclude="**.git" -C cfg .
+tar -czvf cs2.cfg.tgz 
 
